2b2a741792fd27fea604ce92880f00cbb254f689
Ryne Cheow <rynec92@hotmail.com>
2013-03-09 12:56:41 +0800

    image canvas class created

---
+++ src/rocks6205/svgFamily/SVGImageCanvas.java
@@ -0,0 +1,34 @@
+package rocks6205.svgFamily;
+
+import java.awt.image.BufferedImage;
+import java.awt.geom.Rectangle2D;
+
+public class SVGImageCanvas extends BufferedImage {
+	private static double zoomScale = 1;
+
+	public SVGImageCanvas(int width, int height) {
+		super(width, height, BufferedImage.TYPE_4BYTE_ABGR_PRE);
+	}
+
+	public static SVGImageCanvas getBlankCanvas(Rectangle2D.Double bounds) {
+		boolean shouldGetCanvas = bounds != null;
+		shouldGetCanvas &= (bounds.width > 0 && bounds.height > 0);
+		
+		if (shouldGetCanvas) {
+			return new SVGImageCanvas(
+					(int) (SVGImageCanvas.getZoomScale() * Math.ceil(bounds.width)),
+					(int) (SVGImageCanvas.getZoomScale() * Math.ceil(bounds.height)));
+		}
+		
+		return null;
+		
+	}
+
+	public static double getZoomScale() {
+		return zoomScale;
+	}
+
+	public static void setZoomFactor(double zoomScale) {
+		SVGImageCanvas.zoomScale = zoomScale;
+	}
+}
