97f480dca4e75417037e02f7fc0c47930a1f3a11
Ryne Cheow <rynec92@hotmail.com>
2013-03-09 21:15:56 +0800

    Color scheme class added

---
+++ src/rocks6205/svg/adt/SVGColorScheme.java
@@ -0,0 +1,11 @@
+package rocks6205.svg.adt;
+
+import java.awt.Color;
+
+public class SVGColorScheme extends Color {
+
+	public SVGColorScheme(int red, int green, int blue) {
+		super(red, green, blue);
+		// TODO Auto-generated constructor stub
+	}
+}
