c189724416a8837a463088db4b0908f0298b2e0d
Ryne Cheow <rynec92@hotmail.com>
2013-03-09 22:17:48 +0800

    demo merge

--- src/rocks6205/svg/adt/SVGLengthUnit.java
+++ src/rocks6205/svg/adt/SVGLengthUnit.java
@@ -3,7 +3,7 @@ package rocks6205.svg.adt;
 public class SVGLengthUnit {
 
 	private float value;
-	private SVGLengthUnitType type;
+	private SVGLengthUnitType unitType;
 	public SVGLengthUnit() {
 		
 	}
