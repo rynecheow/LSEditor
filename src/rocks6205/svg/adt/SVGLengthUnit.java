98120119f003ed08d682a8ad74861ebe4a505e1b
Ryne Cheow <rynec92@hotmail.com>
2013-03-15 12:03:58 +0800

    unit type in construction

--- src/rocks6205/svg/adt/SVGLengthUnit.java
+++ src/rocks6205/svg/adt/SVGLengthUnit.java
@@ -1,7 +1,10 @@
 package rocks6205.svg.adt;
 
 public class SVGLengthUnit {
-
+	public static final String INT = "[+-]?[0-9]+";
+	public static final String NUM = INT + "([Ee]" + INT
+			+ ")?|[+-]?[0-9]*\\.[0-9]+([Ee]" + INT + ")?";
+	
 	private float value;
 	private SVGLengthUnitType unitType;
 	public SVGLengthUnit() {
--- src/rocks6205/svg/adt/SVGLengthUnitType.java
+++ src/rocks6205/svg/adt/SVGLengthUnitType.java
@@ -1,9 +1,67 @@
 package rocks6205.svg.adt;
 
-public class SVGLengthUnitType {
+import rocks6205.svgFamily.SVGCanvasProperties;
 
-	public SVGLengthUnitType() {
-		// TODO Auto-generated constructor stub
+public enum SVGLengthUnitType {
+	UNKNOWN(null), NUMBER(null), EMS("em"), EXS("ex"), PX("px"), CM("cm"), MM(
+			"mm"), IN("in"), PT("pt"), PC("pc");
+
+	private final String symbol;
+
+	SVGLengthUnitType(String symbol) {
+		this.symbol = symbol;
 	}
 
+	public String getUnitSymbol() {
+		return symbol;
+	}
+/**
+ * mm is set as default unit
+ * In CSS definition, 1pt is 1/72 inch, and 'ex' is the x-height of the font
+ * 
+ * 
+ * @return
+ */
+	public double getUnitScalingFactor() {
+		double mm = 1; 
+		double cm = 10 * mm;
+		double in = 25.4 * mm;
+		double pt = in / 72; 
+		double pc = 12 * pt;
+		double px = 0.75 * pt;
+		double em = SVGCanvasProperties.getFontSize() * px;
+		double ex = 0.5 * em;
+
+		switch (this) {
+		case MM:
+			return mm;
+		case CM:
+			return cm;
+		case IN:
+			return in;
+		case PT:
+			return pt;
+		case PC:
+			return pc;
+		case EMS:
+			return em;
+		case EXS:
+			return ex;
+		case NUMBER:
+		case PX:
+			return px;
+		default:
+			return 0;
+		}
+	}
+
+	public static SVGLengthUnitType getType(String symbol) {
+		for (SVGLengthUnitType type : values()) {
+			if (symbol.equalsIgnoreCase(type.getUnitSymbol())) {
+				return type;
+			}
+		}
+
+		return SVGLengthUnitType.NUMBER;
+	}
 }
