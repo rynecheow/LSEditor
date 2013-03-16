bce00557e167f0ba52be96f6841c792f4dbfe5d2
Ryne Cheow <rynec92@hotmail.com>
2013-03-16 14:16:47 +0800

    Painting class complete

--- src/rocks6205/svg/adt/SVGPainting.java
+++ src/rocks6205/svg/adt/SVGPainting.java
@@ -1,8 +1,7 @@
 /**
  * 
  * Class: SVGPainting
- * Description: Parsing <paint> in SVG documents and convert to floating points with respective type to be
- * 				presented on Java
+ * Description: Parsing <paint> in SVG documents and convert to respective type and color to be drawn on canvas
  * 
  * @author: Cheow Yeong Chi
  * @version: 1.0
@@ -18,34 +17,83 @@ public class SVGPainting {
 	 */
 	SVGColorScheme paintColor;
 	SVGPaintingType paintType;
-	
+
 	/*
 	 * CONSTRUCTORS
 	 */
-	public SVGPainting() {
-		// TODO Auto-generated constructor stub
+	public SVGPainting(SVGPaintingType type) {
+		setPaintType(type);
 	}
-	
-	public SVGPainting(SVGColorScheme color){
-		
+
+	public SVGPainting(SVGColorScheme c){
+		setPaint(c,SVGPaintingType.COLOR);
 	}
-	
+
 	/*
 	 * MUTATOR
 	 */
-	
 	public final void setPaintColor(SVGColorScheme c){
-		this.paintColor = c;
+		if(c!= null)
+			this.paintColor = c;
 	}
 
-	public final void setPaintingType(SVGPaintingType type){
-		this.paintType = type;
+	public final void setPaintType(SVGPaintingType type){
+		if (type != null)   {this.paintType = type;}
+		if (type == SVGPaintingType.NONE)   {this.paintColor = new SVGColorScheme(0, 0, 0);}
 	}
+	
 	/*
 	 * ACCESSOR
 	 */
-	
+	public SVGColorScheme getPaintColor(){
+		return this.paintColor;
+	}
+
+	public SVGPaintingType getPaintType(){
+		return this.paintType;
+	}
+
 	/*
 	 * METHODS
 	 */
+
+	/**
+	 * Parsing paint attribute into types and color that matches the defined types that matches.
+	 * 
+	 * @param paintAttributeString
+	 * @return Painting object that contains respective type and color to be drawn on canvas
+	 */
+	public static SVGPainting parse(String paintAttributeString){
+		paintAttributeString = paintAttributeString.trim();
+		
+		if (paintAttributeString.equalsIgnoreCase(SVGPaintingType.NONE.name())) {
+			return new SVGPainting(SVGPaintingType.NONE);
+		} else if (paintAttributeString.equalsIgnoreCase(SVGPaintingType.CURRENTCOLOR.name())) {
+			return new SVGPainting(SVGPaintingType.CURRENTCOLOR);
+		} else {
+			SVGColorScheme color = SVGColorScheme.parse(paintAttributeString);
+			if (color != null)	{return new SVGPainting(color);}
+		}
+		return null;
+	}
+
+
+	/**
+	 * Setting the type and color of the current paint type.
+	 * @param c
+	 * @param type 
+	 */
+	public final void setPaint(SVGColorScheme c, SVGPaintingType type){
+		setPaintType(type);
+		if (type == SVGPaintingType.COLOR) {setPaintColor(c);}
+	}
+	
+	/**
+	 * Variant. Setting the color of paint.
+	 * 
+	 * @param c Color type object of paint
+	 */
+	public final void setPaint(SVGColorScheme c){
+		setPaint(c, SVGPaintingType.COLOR);
+	}
 }
