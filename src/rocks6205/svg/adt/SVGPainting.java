53e73e6e6c4893f4a97605cf27af83412546db8c
Ryne Cheow <rynec92@hotmail.com>
2013-03-16 13:21:43 +0800

    painting in construction

--- src/rocks6205/svg/adt/SVGPainting.java
+++ src/rocks6205/svg/adt/SVGPainting.java
@@ -1,9 +1,48 @@
+/**
+ * 
+ * Class: SVGPainting
+ * Description: Parsing <paint> in SVG documents and convert to floating points with respective type to be
+ * 				presented on Java
+ * 
+ * @author: Cheow Yeong Chi
+ * @version: 1.0
+ * @date: 16/03/2013
+ * 
+ */
+
 package rocks6205.svg.adt;
 
 public class SVGPainting {
-
+	/*
+	 * PROPERTIES
+	 */
+	SVGColorScheme paintColor;
+	SVGPaintingType paintType;
+	
+	/*
+	 * CONSTRUCTORS
+	 */
 	public SVGPainting() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public SVGPainting(SVGColorScheme color){
+		
+	}
+	
+	/*
+	 * MUTATOR
+	 */
+	
+	public final void setPaintColor(SVGColorScheme c){
+		this.paintColor = c;
+	}
 
+	/*
+	 * ACCESSOR
+	 */
+	
+	/*
+	 * METHODS
+	 */
 }
--- src/rocks6205/svg/adt/SVGPaintingType.java
+++ src/rocks6205/svg/adt/SVGPaintingType.java
@@ -1,3 +1,14 @@
+/**
+ * 
+ * Class: SVGPaintingType
+ * Description: Enumerates painting type recognized by data type <paint> in SVG
+ * 
+ * @author: Cheow Yeong Chi
+ * @version: 1.0
+ * @date: 16/03/2013
+ * 
+ */
+
 package rocks6205.svg.adt;
 
 public enum SVGPaintingType {
