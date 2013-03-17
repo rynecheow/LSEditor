4061d930443038f1f1b760ddb553d12f787c7e11
Ryne Cheow <rynec92@hotmail.com>
2013-03-17 18:38:35 +0800

    forbid main to run

--- src/rocks6205/svgFamily/Main.java
+++ src/rocks6205/svgFamily/Main.java
@@ -7,22 +7,22 @@ import rocks6205.svg.engine.SVGViewController;
 
 public class Main {
 
-	public static void main(String []rcks){
-
-		String fileURI = "";
-
-		SVGModel m = new SVGModel();
-		SVGView v = new SVGView();
-		SVGViewController c = new SVGViewController(m, v);
-		m.addObserver(v);
-		v.setModel(m);
-		v.setController(c);
-		v.setVisible(true);
-		if (rcks.length > 0) {
-			fileURI = rcks[0];
-			//			load file
-			//			c.fileLoad(new File(fileURI));
-		}
-	}
+//	public static void main(String []rcks){
+//
+//		String fileURI = "";
+//
+//		SVGModel m = new SVGModel();
+//		SVGView v = new SVGView();
+//		SVGViewController c = new SVGViewController(m, v);
+//		m.addObserver(v);
+//		v.setModel(m);
+//		v.setController(c);
+//		v.setVisible(true);
+//		if (rcks.length > 0) {
+//			fileURI = rcks[0];
+//			//			load file
+//			//			c.fileLoad(new File(fileURI));
+//		}
+//	}
 
 }
