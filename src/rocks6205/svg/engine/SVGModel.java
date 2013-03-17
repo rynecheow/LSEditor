37091110a23daca4c0a9d4453e32426b2ae133e1
Ryne Cheow <rynec92@hotmail.com>
2013-03-17 18:29:48 +0800

    getter setters

--- src/rocks6205/svg/engine/SVGModel.java
+++ src/rocks6205/svg/engine/SVGModel.java
@@ -11,16 +11,45 @@
 
 package rocks6205.svg.engine;
 
+import java.util.Observable;
+
 import rocks6205.svg.elements.SVGSVGElement;
 import rocks6205.svgFamily.SVGImageCanvas;
 
-public class SVGModel {
+public class SVGModel extends Observable{
 
-	private SVGSVGElement svgElement;
+	private SVGSVGElement SVGElement;
 	private SVGImageCanvas imageCanvas;
-	
+
 	public SVGModel() {
 		// TODO Auto-generated constructor stub
 	}
 
+
+	/*
+	 * ACCESSOR
+	 */
+	public SVGSVGElement getSVGElement() {
+		return SVGElement;
+	}
+
+	public SVGImageCanvas getImageCanvas() {
+		return imageCanvas;
+	}
+	/*
+	 * MUTATORS
+	 */
+	public void setImageCanvas(SVGImageCanvas imageCanvas) {
+		this.imageCanvas = imageCanvas;
+	}
+
+	public void setSVGElement(SVGSVGElement sVGElement) {
+		SVGElement = sVGElement;
+	}
+	/*
+	 * OVERRIDING
+	 */
+	public void addObserver(SVGView v) {
+
+	}
 }
--- src/rocks6205/svg/engine/SVGViewController.java
+++ src/rocks6205/svg/engine/SVGViewController.java
@@ -10,14 +10,87 @@
  */
 
 package rocks6205.svg.engine;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class SVGViewController {
-
+public class SVGViewController implements MouseMotionListener, MouseListener {
+	/*
+	 * PROPERTIES
+	 */
 	private SVGModel model;
 	private SVGView view;
-	
-	public SVGViewController() {
-		// TODO Auto-generated constructor stub
+
+	/*
+	 * CONSTRUCTOR
+	 */
+	public SVGViewController(SVGModel model, SVGView view) {
+		setModel(model);
+		setView(view);
 	}
 
+	/*
+	 * ACCESSORS
+	 */
+	public SVGView getView() {
+		return view;
+	}
+
+	public SVGModel getModel() {
+		return model;
+	}
+
+	/*
+	 * MUTATORS
+	 */
+	public void setModel(SVGModel model) {
+		this.model = model;
+	}
+
+
+	public void setView(SVGView view) {
+		this.view = view;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.awt.event.MouseListener
+	 * 
+	 * Override
+	 */
+
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
