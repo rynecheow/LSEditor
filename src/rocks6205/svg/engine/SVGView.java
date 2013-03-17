c78fca4805c533896dce8e20b388a0e45ba082f5
Crystal002 <hueyjing_toh@hotmail.com>
2013-03-17 18:10:42 +0800

    Temporary border show general layout of final GUI

--- src/rocks6205/svg/engine/SVGView.java
+++ src/rocks6205/svg/engine/SVGView.java
@@ -1,20 +1,76 @@
 /**
- * 
+ *
  * Class: SVGView
- * Description: 
- * 
- * @author: Cheow Yeong Chi
+ * Description: General layout of the GUI
+ *
+ * @author: Toh Huey Jing
  * @version: 1.0
- * @date: 14/03/2013
- * 
+ * @date: 17/03/2013
+ *
  */
 
 package rocks6205.svg.engine;
 
-public class SVGView {
+import java.awt.*;
+import javax.swing.*;
+
+public class SVGView extends JFrame {
+
+	JPanel panel, panelTop, panelLeft, panelRight, panelBottom;
+	JPanel inPanel, inPanelTop, inPanelLeft, inPanelRight, inPanelBottom;
 
 	public SVGView() {
-		// TODO Auto-generated constructor stub
+
+		panel = new JPanel();
+		panelTop = new JPanel();
+		panelLeft = new JPanel();
+		panelRight = new JPanel();
+		panelBottom = new JPanel();
+		
+		inPanel = new JPanel();
+		inPanelTop = new JPanel();
+		inPanelLeft = new JPanel();
+		inPanelRight = new JPanel();
+		inPanelBottom = new JPanel();
+
+		Container c = getContentPane();
+		
+		c.setLayout(new BorderLayout());
+		panel.setLayout(new BorderLayout());
+		
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));
+		panelTop.setBorder(BorderFactory.createLineBorder(Color.black));
+		panelLeft.setBorder(BorderFactory.createLineBorder(Color.black));
+		panelRight.setBorder(BorderFactory.createLineBorder(Color.black));
+		panelBottom.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		inPanel.setBorder(BorderFactory.createLineBorder(Color.blue));
+		inPanelTop.setBorder(BorderFactory.createLineBorder(Color.blue));
+		inPanelLeft.setBorder(BorderFactory.createLineBorder(Color.blue));
+		inPanelRight.setBorder(BorderFactory.createLineBorder(Color.blue));
+		inPanelBottom.setBorder(BorderFactory.createLineBorder(Color.blue));
+		
+		panel.add(inPanel);
+		panel.add(inPanelTop, BorderLayout.NORTH);
+		panel.add(inPanelLeft, BorderLayout.WEST);
+		panel.add(inPanelRight, BorderLayout.EAST);
+		panel.add(inPanelBottom, BorderLayout.SOUTH);
+		
+		c.add(panel);
+		c.add(panelTop, BorderLayout.NORTH);
+		c.add(panelLeft, BorderLayout.WEST);
+		c.add(panelRight, BorderLayout.EAST);
+		c.add(panelBottom, BorderLayout.SOUTH);
+		
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		setTitle("SVG Editor");
+		setVisible(true);
+		setResizable(false);
+	}
+
+	public static void main (String args[]) {
+		SVGView view = new SVGView();
 	}
 
 }
