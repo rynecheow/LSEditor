a48a6101e08ce7c2225edce0034b924f4b088bbd
Ryne Cheow <rynec92@hotmail.com>
2013-03-17 19:08:05 +0800

    abstract action class

---
+++ src/rocks6205/svg/engine/events/SVGViewAction.java
@@ -0,0 +1,30 @@
+package rocks6205.svg.engine.events;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+public abstract class SVGViewAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9085407834848872724L;
+	/*
+	 * CONSTRUCTOR
+	 */
+	public SVGViewAction(String actionName, String tooltipText,
+			Integer mnemonic, KeyStroke keyStroke) {
+		putValue(Action.NAME,actionName);
+		putValue(SHORT_DESCRIPTION, tooltipText);
+		putValue(MNEMONIC_KEY, mnemonic);
+		putValue(ACCELERATOR_KEY, keyStroke);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
