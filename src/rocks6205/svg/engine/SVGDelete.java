97eb37629775d12b1cea807617f7dc350d64c091
Crystal002 <hueyjing_toh@hotmail.com>
2013-03-18 10:56:48 +0800

    Panel for delete button.

---
+++ src/rocks6205/svg/engine/SVGDelete.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * Class: SVGDelete
+ * Description: JPanel containing the Delete button.
+ * 
+ * @author: Toh Huey Jing
+ * @version: 1.0
+ * @date: 18/03/2013
+ * 
+ */
+
+package rocks6205.svg.engine;
+
+import javax.swing.*;
+
+public class SVGDelete extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * GUI COMPONENTS
+	 */
+	JButton delBut;
+
+	/*
+	 * CONSRTUCTOR
+	 */
+	public SVGDelete() {
+		
+		delBut = new JButton("Delete");
+		
+		this.add(delBut);
+		
+	}
+
+}
