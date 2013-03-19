61a5c6ab63bd42bff4aa29b23d2a3692c8eec674
Ryne Cheow <rynec92@hotmail.com>
2013-03-19 20:14:34 +0800

    line class

--- src/rocks6205/svg/elements/SVGLineElement.java
+++ src/rocks6205/svg/elements/SVGLineElement.java
@@ -11,10 +11,18 @@
 
 package rocks6205.svg.elements;
 
+import java.awt.BasicStroke;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Double;
+
+import org.w3c.dom.Element;
 
 import rocks6205.svg.adt.SVGLengthUnit;
 import rocks6205.svg.adt.SVGPainting;
+import rocks6205.svg.adt.SVGPaintingType;
 import rocks6205.svgFamily.SVGImageCanvas;
 
 public class SVGLineElement extends SVGGenericElement {
@@ -28,7 +36,7 @@ public class SVGLineElement extends SVGGenericElement {
 	private SVGLengthUnit y2 = new SVGLengthUnit(0);
 
 	/*
-	 * CONSTRUCTOR
+	 * CONSTRUCTORS
 	 */
 	public SVGLineElement( SVGLengthUnit x1 , SVGLengthUnit y1 , SVGLengthUnit x2 , SVGLengthUnit y2 , 
 			SVGPainting stroke , SVGPainting strokeWidth ) {
@@ -38,6 +46,7 @@ public class SVGLineElement extends SVGGenericElement {
 		this.y2 = y2;
 	}
 
+	public SVGLineElement(){}
 	/*
 	 * ACCESSORS
 	 */
@@ -75,40 +84,87 @@ public class SVGLineElement extends SVGGenericElement {
 	/**
 	 * @param x1 contains value for field x1
 	 */
-	public void setX( SVGLengthUnit x1 ) {
-		this.x1 = x1;
+	public void setX1( SVGLengthUnit x1 ) {
+		if(x1 != null)	this.x1 = x1;
 	}
 
 	/**
 	 * @param y1 contains value for field y1
 	 */
-	public void setY( SVGLengthUnit y1 ) {
-		this.y1 = y1;
+	public void setY1( SVGLengthUnit y1 ) {
+		if(y1 != null)	this.y1 = y1;
 	}
 
 	/**
 	 * @param x2 contains value for field x2
 	 */
-	public void setWidth( SVGLengthUnit x2 ) {
-		this.x2 = x2;
+	public void setX2( SVGLengthUnit x2 ) {
+		if(x2 != null)	this.x2 = x2;
 	}
 
 	/**
 	 * @param y2 contains value for field y2
 	 */
 	public void setY2( SVGLengthUnit y2 ) {
-		this.y2 = y2;
+		if(y2!= null)	this.y2 = y2;
 	}
 
 	@Override
 	public Double getBounds() {
-		// TODO Auto-generated method stub
-		return null;
+		float padding = 0;
+		if (getStroke().getPaintType() != SVGPaintingType.NONE) {
+			padding = getStrokeWidth().getValue() / 2;
+		}
+		float computedX = Math.min(x1.getValue(), x2.getValue()) - padding;
+		float computedY = Math.min(y1.getValue(), y2.getValue()) - padding;
+		float computedWidth = Math.abs(x2.getValue() - x1.getValue()) + 2
+				* padding;
+		float computedHeight = Math.abs(y2.getValue() - y1.getValue()) + 2
+				* padding;
+
+		return new Rectangle2D.Double(computedX, computedY, computedWidth,
+				computedHeight);
 	}
 
 	@Override
 	public SVGImageCanvas draw() {
-		// TODO Auto-generated method stub
-		return null;
+		Rectangle2D.Double bounds = getBounds();
+		SVGImageCanvas canvas = SVGImageCanvas.getBlankCanvas(bounds);
+		Graphics2D graphics = canvas.createGraphics();
+		Line2D.Double line = new Line2D.Double(x1.getValue() - bounds.x,
+				y1.getValue() - bounds.y, x2.getValue() - bounds.x,
+				y2.getValue() - bounds.y);
+
+		graphics.scale(SVGImageCanvas.getZoomScale(), SVGImageCanvas.getZoomScale());
+		graphics.setPaint(getStroke().getPaintColor());
+		graphics.setStroke(new BasicStroke((float) getStrokeWidth().getValue(),
+				getStrokeLineCap(), getStrokeLineJoin()));
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics.draw(line);
+
+		return canvas;
+	}
+	
+	/*
+	 * METHOD
+	 */
+	/**
+	 * Parse line element from DOM level to object interpretable as defined
+	 * 
+	 * @param e		Element get from the XML Parser
+	 * @return		Line element parsed from Element to be drawn
+	 */
+	public static SVGLineElement parseElement(Element e) {
+		SVGLineElement line = new SVGLineElement();
+
+		line.setX1(SVGLengthUnit.parse(e.getAttributeNS(null, "x1")));
+		line.setY1(SVGLengthUnit.parse(e.getAttributeNS(null, "y1")));
+		line.setX2(SVGLengthUnit.parse(e.getAttributeNS(null, "x2")));
+		line.setY2(SVGLengthUnit.parse(e.getAttributeNS(null, "y2")));
+		
+		line.parseAttributes(e);
+
+		return line;
 	}
 }
