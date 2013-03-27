a467bce98f074dbc9ed50e960bd64c9c365a86cd
Ryne Cheow <rynec92@hotmail.com>
2013-03-27 23:41:55 +0800

    rect element refactored

--- src/rocks6205/svg/elements/SVGLineElement.java
+++ src/rocks6205/svg/elements/SVGLineElement.java
@@ -2,18 +2,20 @@ package rocks6205.svg.elements;
 
 //~--- non-JDK imports --------------------------------------------------------
 
+import org.w3c.dom.Element;
+
+import rocks6205.svg.adt.SVGLengthUnit;
+import rocks6205.svg.adt.SVGPaintingType;
+
+import rocks6205.svgFamily.SVGImageCanvas;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-
-import org.w3c.dom.Element;
-
-import rocks6205.svg.adt.SVGLengthUnit;
-import rocks6205.svg.adt.SVGPaintingType;
-import rocks6205.svgFamily.SVGImageCanvas;
-//~--- JDK imports ------------------------------------------------------------
 
 /**
  * The <code>SVGLineElement</code> class is used to parse <<code>line</code>> element in
@@ -21,7 +23,7 @@ import rocks6205.svgFamily.SVGImageCanvas;
  * the image canvas.
  *
  * @author Sugar CheeSheen Chan
- * 
+ *
  * @since 1.1
  *
  */
@@ -34,76 +36,84 @@ public class SVGLineElement extends SVGGenericElement {
     /**
      * x-coordinate of the first point
      */
-    private SVGLengthUnit x1 = new SVGLengthUnit(0);
+    private SVGLengthUnit x1;
 
     /**
      * y-coordinate of the first point
      */
-    private SVGLengthUnit y1 = new SVGLengthUnit(0);
+    private SVGLengthUnit y1;
 
     /**
      * x-coordinate of the second point
      */
-    private SVGLengthUnit x2 = new SVGLengthUnit(0);
+    private SVGLengthUnit x2;
 
     /**
      * y-coordinate of the second point
      */
-    private SVGLengthUnit y2 = new SVGLengthUnit(0);
+    private SVGLengthUnit y2;
 
     /**
      * Default constructor.
+     * Construct an instance of <code>SVGLineElement</code> with coordinates initialised to zero.
      */
-    public SVGLineElement() {}
+    public SVGLineElement() {
+        x1 = new SVGLengthUnit(0);
+        y1 = new SVGLengthUnit(0);
+        x2 = new SVGLengthUnit(0);
+        y2 = new SVGLengthUnit(0);
+    }
 
     /*
      * CONSTRUCTORS
      */
+
     /**
      * Construct an instance of <code>SVGLineElement</code> with the coordinates
      * of the first point and the second point.
-     * 
+     *
      * @param x1 x-coordinate of the first point
      * @param y1 y-coordinate of the first point
      * @param x2 x-coordinate of the second point
      * @param y2 y-coordinate of the second point
      */
     public SVGLineElement(SVGLengthUnit x1, SVGLengthUnit y1, SVGLengthUnit x2, SVGLengthUnit y2) {
-	setX1(x1);
-	setY1(y1);
-	setX2(x2);
-	setY2(y2);
+        setX1(x1);
+        setY1(y1);
+        setX2(x2);
+        setY2(y2);
     }
 
     /*
      * ACCESSORS
      */
+
     /**
      * @return x1 x-coordinate of the first point
      */
     public SVGLengthUnit getX1() {
-	return this.x1;
+        return this.x1;
     }
 
     /**
      * @return y1 y-coordinate of the first point
      */
     public SVGLengthUnit getY1() {
-	return this.y1;
+        return this.y1;
     }
 
     /**
      * @return x2 x-coordinate of the second point
      */
     public SVGLengthUnit getX2() {
-	return this.x2;
+        return this.x2;
     }
 
     /**
      * @return y2 y-coordinate of the second point
      */
     public SVGLengthUnit getY2() {
-	return this.y2;
+        return this.y2;
     }
 
     /*
@@ -114,77 +124,77 @@ public class SVGLineElement extends SVGGenericElement {
      * @param x1 x-coordinate of the first point
      */
     public void setX1(SVGLengthUnit x1) {
-	if (x1 != null) {
-	    this.x1 = x1;
-	}
+        if (x1 != null) {
+            this.x1 = x1;
+        }
     }
 
     /**
      * @param y1 y-coordinate of the first point
      */
     public void setY1(SVGLengthUnit y1) {
-	if (y1 != null) {
-	    this.y1 = y1;
-	}
+        if (y1 != null) {
+            this.y1 = y1;
+        }
     }
 
     /**
      * @param x2 x-coordinate of the second point
      */
     public void setX2(SVGLengthUnit x2) {
-	if (x2 != null) {
-	    this.x2 = x2;
-	}
+        if (x2 != null) {
+            this.x2 = x2;
+        }
     }
 
     /**
      * @param y2 y-coordinate of the second point
      */
     public void setY2(SVGLengthUnit y2) {
-	if (y2 != null) {
-	    this.y2 = y2;
-	}
+        if (y2 != null) {
+            this.y2 = y2;
+        }
     }
 
-   /**
-    * {@inheritDoc}
-    */
+    /**
+     * {@inheritDoc}
+     */
     public Rectangle2D.Float getBounds() {
-	float padding = 0;
+        float padding = 0;
 
-	if (getStroke().getPaintType() != SVGPaintingType.NONE) {
-	    padding = getStrokeWidth().getValue() / 2;
-	}
+        if (getStroke().getPaintType() != SVGPaintingType.NONE) {
+            padding = getStrokeWidth().getValue() / 2;
+        }
 
-	float computedX      = Math.min(x1.getValue(), x2.getValue()) - padding;
-	float computedY      = Math.min(y1.getValue(), y2.getValue()) - padding;
-	float computedWidth  = Math.abs(x2.getValue() - x1.getValue()) + 2 * padding;
-	float computedHeight = Math.abs(y2.getValue() - y1.getValue()) + 2 * padding;
+        float computedX      = Math.min(x1.getValue(), x2.getValue()) - padding;
+        float computedY      = Math.min(y1.getValue(), y2.getValue()) - padding;
+        float computedWidth  = Math.abs(x2.getValue() - x1.getValue()) + 2 * padding;
+        float computedHeight = Math.abs(y2.getValue() - y1.getValue()) + 2 * padding;
 
-	return new Rectangle2D.Float(computedX, computedY, computedWidth, computedHeight);
+        return new Rectangle2D.Float(computedX, computedY, computedWidth, computedHeight);
     }
 
     /**
      * {@inheritDoc}
      */
     public SVGImageCanvas draw() {
-	Rectangle2D.Float bounds = getBounds();
-	SVGImageCanvas    canvas = SVGImageCanvas.getBlankCanvas(bounds);
+        Rectangle2D.Float bounds = getBounds();
+        SVGImageCanvas    canvas = SVGImageCanvas.getBlankCanvas(bounds);
 
-	if (canvas != null) {
-	    Graphics2D    graphics = canvas.createGraphics();
-	    Line2D.Double line     = new Line2D.Double(x1.getValue() - bounds.x, y1.getValue() - bounds.y,
-		    x2.getValue() - bounds.x, y2.getValue() - bounds.y);
+        if (canvas != null) {
+            Graphics2D    graphics = canvas.createGraphics();
+            Line2D.Double line     = new Line2D.Double(x1.getValue() - bounds.x, y1.getValue() - bounds.y,
+                                         x2.getValue() - bounds.x, y2.getValue() - bounds.y);
 
-	    graphics.scale(SVGImageCanvas.getZoomScale(), SVGImageCanvas.getZoomScale());
-	    graphics.setPaint(getStroke().getPaintColor());
-	    graphics.setStroke(new BasicStroke((float) getStrokeWidth().getValue(), getStrokeLineCap(),
-		    getStrokeLineJoin()));
-	    graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	    graphics.draw(line);
-	}
+            graphics.scale(SVGImageCanvas.getZoomScale(), SVGImageCanvas.getZoomScale());
+            graphics.setPaint(getStroke().getPaintColor());
+            graphics.setStroke(new BasicStroke((float) getStrokeWidth().getValue(), getStrokeLineCap(),
+                                               getStrokeLineJoin()));
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            graphics.draw(line);
+        }
 
-	return canvas;
+        return canvas;
     }
 
     /*
@@ -194,21 +204,18 @@ public class SVGLineElement extends SVGGenericElement {
     /**
      * Parse line element from DOM level to object interpretable as defined
      *
-     * @param e         Element get from the XML Parser
-     * @return          Line element parsed from Element to be drawn
+     * @param element         Element get from the XML Parser
+     * @return                Line element parsed from Element to be drawn
      */
-    public static SVGLineElement parseElement(Element e) {
-	SVGLineElement line = new SVGLineElement();
+    public static SVGLineElement parseElement(Element element) {
+        SVGLineElement line = new SVGLineElement();
 
-	line.setX1(SVGLengthUnit.parse(e.getAttributeNS(null, "x1")));
-	line.setY1(SVGLengthUnit.parse(e.getAttributeNS(null, "y1")));
-	line.setX2(SVGLengthUnit.parse(e.getAttributeNS(null, "x2")));
-	line.setY2(SVGLengthUnit.parse(e.getAttributeNS(null, "y2")));
-	line.parseAttributes(e);
+        line.setX1(SVGLengthUnit.parse(element.getAttributeNS(null, "x1")));
+        line.setY1(SVGLengthUnit.parse(element.getAttributeNS(null, "y1")));
+        line.setX2(SVGLengthUnit.parse(element.getAttributeNS(null, "x2")));
+        line.setY2(SVGLengthUnit.parse(element.getAttributeNS(null, "y2")));
+        line.parseAttributes(element);
 
-	return line;
+        return line;
     }
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
--- src/rocks6205/svg/elements/SVGRectElement.java
+++ src/rocks6205/svg/elements/SVGRectElement.java
@@ -1,159 +1,224 @@
-/**
- * 
- * Class: SVGRectElement
- * Description: 
- * 
- * @author: Sugar CheeSheen Chan
- * @date: 15/03/2013
- * 
- */
-
 package rocks6205.svg.elements;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.w3c.dom.Element;
+
+import rocks6205.svg.adt.SVGLengthUnit;
+import rocks6205.svg.adt.SVGPaintingType;
+
+import rocks6205.svgFamily.SVGImageCanvas;
+
+//~--- JDK imports ------------------------------------------------------------
 
 import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
 
-import org.w3c.dom.Element;
-
-import rocks6205.svg.adt.SVGLengthUnit;
-import rocks6205.svg.adt.SVGPaintingType;
-import rocks6205.svgFamily.SVGImageCanvas;
-
+/**
+ * The <code>SVGRectElement</code> class is used to parse <<code>rect</code>> element in
+ * SVG documents and convert to <code>SVGRectElement</code> which is subsequently drawn on
+ * the image canvas.
+ *
+ * @author Sugar CheeSheen Chan
+ *
+ * @since 1.1
+ *
+ */
 public class SVGRectElement extends SVGGenericElement {
 
     /*
      * PROPERTIES
      */
-    private SVGLengthUnit x = new SVGLengthUnit( 0 );
-    private SVGLengthUnit y = new SVGLengthUnit( 0 );
-    private SVGLengthUnit width = new SVGLengthUnit( 0 );
-    private SVGLengthUnit height = new SVGLengthUnit( 0 );
 
-    /*
-     * CONSTRUCTOR
+    /**
+     * x-axis coordinate of the side of the rectangle which has the smaller x-axis coordinate
+     * value in the current user coordinate system.
      */
-    public SVGRectElement( SVGLengthUnit x , SVGLengthUnit y , SVGLengthUnit width , SVGLengthUnit height) {
-	setX(x);
-	setY(y);
-	setWidth(width);
-	setHeight(height);
+    private SVGLengthUnit x;
+
+    /**
+     * y-axis coordinate of the side of the rectangle which has the smaller y-axis coordinate
+     * value in the current user coordinate system.
+     */
+    private SVGLengthUnit y;
+
+    /**
+     *  Width of the rectangle
+     */
+    private SVGLengthUnit width;
+
+    /**
+     * Height of the rectangle.
+     */
+    private SVGLengthUnit height;
+
+    /**
+     * Default constructor.<p>
+     * Construct an instance of <code>SVGRectElement</code> with coordinates and lengths initialised to zero.
+     */
+    public SVGRectElement() {
+        x      = new SVGLengthUnit(0);
+        y      = new SVGLengthUnit(0);
+        width  = new SVGLengthUnit(0);
+        height = new SVGLengthUnit(0);
     }
-    public SVGRectElement(){}
+
+    /**
+     * Construct an instance of <code>SVGRectElement</code> with the coordinates
+     * of the first point and the second point.
+     *
+     * @param x x-coordinate of rectangle
+     * @param y y-coordinate of rectangle
+     * @param width Width of the rectangle
+     * @param height Height of the rectangle
+     */
+    public SVGRectElement(SVGLengthUnit x, SVGLengthUnit y, SVGLengthUnit width, SVGLengthUnit height) {
+        setX(x);
+        setY(y);
+        setWidth(width);
+        setHeight(height);
+    }
+
     /*
      * ACCESSORS
      */
+
     /**
-     * @return Value of x
+     * @return x x-coordinate of rectangle
      */
     public SVGLengthUnit getX() {
-	return this.x;
+        return this.x;
     }
 
     /**
-     * @return Value of y
+     * @return y y-coordinate of rectangle
      */
     public SVGLengthUnit getY() {
-	return this.y;
+        return this.y;
     }
 
     /**
-     * @return Value of width
+     * @return width Width of the rectangle
      */
     public SVGLengthUnit getWidth() {
-	return this.width;
+        return this.width;
     }
 
     /**
-     * @return Value of height
+     * @return height Height of the rectangle
      */
     public SVGLengthUnit getHeight() {
-	return this.height;
+        return this.height;
     }
 
     /*
      * MUTATORS
      */
+
     /**
-     * @param x contains value for field x
+     * @param x x-coordinate of rectangle
      */
-    public void setX( SVGLengthUnit x ) {
-	if (x != null)	this.x = x;
+    public void setX(SVGLengthUnit x) {
+        if (x != null) {
+            this.x = x;
+        }
     }
 
     /**
-     * @param y contains value for field y
+     * @param y y-coordinate of rectangle
      */
-    public void setY( SVGLengthUnit y ) {
-	if (y != null)	this.y = y;
+    public void setY(SVGLengthUnit y) {
+        if (y != null) {
+            this.y = y;
+        }
     }
 
     /**
-     * @param width contains value for field width
+     * @param width Width of the rectangle
      */
-    public void setWidth( SVGLengthUnit width ) {
-	boolean shouldSetWidth = width!= null && width.getValue() >= 0 ;
-	if (shouldSetWidth)	this.width = width;
+    public void setWidth(SVGLengthUnit width) {
+        boolean shouldSetWidth = (width != null) && (width.getValue() >= 0);
+
+        if (shouldSetWidth) {
+            this.width = width;
+        }
     }
 
     /**
-     * @param height contains value for field height
+     * @param height Height of the rectangle
      */
-    public void setHeight( SVGLengthUnit height ) {
-	boolean shouldSetHeight = height!= null && height.getValue() >= 0;
-	if (shouldSetHeight)	this.height = height;
+    public void setHeight(SVGLengthUnit height) {
+        boolean shouldSetHeight = (height != null) && (height.getValue() >= 0);
+
+        if (shouldSetHeight) {
+            this.height = height;
+        }
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public Rectangle2D.Float getBounds() {
-	float w = width.getValue();
-	float h = height.getValue();
-	boolean valid = (w > 0) && (h > 0);
+        float   w     = width.getValue();
+        float   h     = height.getValue();
+        boolean valid = (w > 0) && (h > 0);
 
-	if(valid){
-	    float padding = 0;
-	    if (getStroke().getPaintType() != SVGPaintingType.NONE) {
-		padding = getStrokeWidth().getValue() / 2;
-	    }
+        if (valid) {
+            float padding = 0;
 
-	    return new Rectangle2D.Float( x.getValue() - padding , 
-		    y.getValue() - padding , w + 2 * padding , h + 2 * padding );
-	}else{
-	    return null;
-	}
+            if (getStroke().getPaintType() != SVGPaintingType.NONE) {
+                padding = getStrokeWidth().getValue() / 2;
+            }
+
+            return new Rectangle2D.Float(x.getValue() - padding, y.getValue() - padding, w + 2 * padding,
+                                         h + 2 * padding);
+        } else {
+            return null;
+        }
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public SVGImageCanvas draw() {
-	Rectangle2D.Float bounds = getBounds();
-	SVGImageCanvas canvas = SVGImageCanvas.getBlankCanvas(bounds);
-	if(canvas != null){
-	    Graphics2D graphics = canvas.createGraphics();
+        Rectangle2D.Float bounds = getBounds();
+        SVGImageCanvas    canvas = SVGImageCanvas.getBlankCanvas(bounds);
 
-	    Rectangle2D rect = new Rectangle2D.Double( x.getValue() - bounds.x , 
-		    y.getValue() - bounds.y , width.getValue() , height.getValue() );
-	    graphics.scale(SVGImageCanvas.getZoomScale(), SVGImageCanvas.getZoomScale());
-	    graphics.setStroke( new BasicStroke((float)getStrokeWidth().getValue() , getStrokeLineCap(), getStrokeLineJoin() ) );
-	    graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-		    RenderingHints.VALUE_ANTIALIAS_ON);
-	    graphics.setPaint( getFill().getPaintColor() );
-	    graphics.fill( rect );
-	    graphics.setPaint( getStroke().getPaintColor() );
-	    graphics.draw( rect );
-	}
-	return canvas;
+        if (canvas != null) {
+            Graphics2D  graphics = canvas.createGraphics();
+            Rectangle2D rect     = new Rectangle2D.Double(x.getValue() - bounds.x, y.getValue() - bounds.y,
+                                       width.getValue(), height.getValue());
+
+            graphics.scale(SVGImageCanvas.getZoomScale(), SVGImageCanvas.getZoomScale());
+            graphics.setStroke(new BasicStroke((float) getStrokeWidth().getValue(), getStrokeLineCap(),
+                                               getStrokeLineJoin()));
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            graphics.setPaint(getFill().getPaintColor());
+            graphics.fill(rect);
+            graphics.setPaint(getStroke().getPaintColor());
+            graphics.draw(rect);
+        }
+
+        return canvas;
     }
 
+    /**
+     * Parse rectangle element from DOM level to object interpretable as defined
+     *
+     * @param element   Element get from the XML Parser
+     * @return          Rectangle element to be drawn parsed from <code>e</code>
+     */
     public static SVGRectElement parseElement(Element element) {
-	SVGRectElement rect = new SVGRectElement();
+        SVGRectElement rect = new SVGRectElement();
 
-	rect.setX(SVGLengthUnit.parse(element.getAttributeNS(null, "x")));
-	rect.setY(SVGLengthUnit.parse(element.getAttributeNS(null, "y")));
-	rect.setWidth(SVGLengthUnit.parse(element.getAttributeNS(null, "width")));
-	rect.setHeight(SVGLengthUnit.parse(element.getAttributeNS(null, "height")));
-	rect.parseAttributes(element);
+        rect.setX(SVGLengthUnit.parse(element.getAttributeNS(null, "x")));
+        rect.setY(SVGLengthUnit.parse(element.getAttributeNS(null, "y")));
+        rect.setWidth(SVGLengthUnit.parse(element.getAttributeNS(null, "width")));
+        rect.setHeight(SVGLengthUnit.parse(element.getAttributeNS(null, "height")));
+        rect.parseAttributes(element);
 
-	return rect;
+        return rect;
     }
 }
