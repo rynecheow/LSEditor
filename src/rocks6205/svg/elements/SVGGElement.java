a2ce894f04a04fd2d9f9c404053ead848d781ac7
Ryne Cheow <rynec92@hotmail.com>
2013-03-28 00:20:04 +0800

    refactor ELEMENTS package complete

--- src/rocks6205/svg/elements/SVGGElement.java
+++ src/rocks6205/svg/elements/SVGGElement.java
@@ -1,88 +1,110 @@
-/**
- * 
- * Class: SVGGElement
- * Description: 
- * 
- * @author: Cheow Yeong Chi
- * @date: 14/03/2013
- * 
- */
-
 package rocks6205.svg.elements;
 
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
+//~--- non-JDK imports --------------------------------------------------------
 
 import org.w3c.dom.Element;
 
 import rocks6205.svgFamily.SVGImageCanvas;
 
+//~--- JDK imports ------------------------------------------------------------
+
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * The <code>SVGGElement</code> class is a container used to group objects
+ * corresponds to the <<code>g</code>> element in the SVG document.
+ * Transformations applied to the <<code>g</code>> element are performed on all of its child
+ * elements.
+ *
+ * @author Cheow Yeong Chi
+ * @since 1.1
+ *
+ */
 public class SVGGElement extends SVGContainerElement {
 
     /*
      * CONSTRUCTOR
      */
+
+    /**
+     * Default constructor
+     */
     public SVGGElement() {}
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public Rectangle2D.Float getBounds() {
-	Rectangle2D.Float descendantRect = null, rect = null;
-	float computedX = 0;
-	float computedY = 0;
-	float computedWidth = 0;
-	float computedHeight = 0;
+        Rectangle2D.Float descendantRect = null,
+                          rect           = null;
+        float             computedX      = 0;
+        float             computedY      = 0;
+        float             computedWidth  = 0;
+        float             computedHeight = 0;
 
-	for (SVGGenericElement descendants : getDescendants()) {
-	    descendantRect = descendants.getBounds();
-	    if (descendantRect != null) {
-		if (rect == null) {
-		    rect = descendantRect;
-		} else {
-		    Rectangle2D.union(descendantRect, rect, rect);
-		}
+        for (SVGGenericElement descendants : getDescendants()) {
+            descendantRect = descendants.getBounds();
 
-		computedX = rect.x;
-		computedY = rect.y;
-		computedWidth = rect.width;
-		computedHeight = rect.height;
-	    }
-	}
+            if (descendantRect != null) {
+                if (rect == null) {
+                    rect = descendantRect;
+                } else {
+                    Rectangle2D.union(descendantRect, rect, rect);
+                }
 
-	if (rect != null) {
-	    return new Rectangle2D.Float(computedX, computedY, computedWidth,
-		    computedHeight);
-	} else {
-	    return null;
-	}
+                computedX      = rect.x;
+                computedY      = rect.y;
+                computedWidth  = rect.width;
+                computedHeight = rect.height;
+            }
+        }
+
+        if (rect != null) {
+            return new Rectangle2D.Float(computedX, computedY, computedWidth, computedHeight);
+        } else {
+            return null;
+        }
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public SVGImageCanvas draw() {
-	Rectangle2D.Float bounds = getBounds(), descendantRect;
-	SVGImageCanvas canvas = SVGImageCanvas.getBlankCanvas(bounds);
+        Rectangle2D.Float bounds = getBounds(), descendantRect;
+        SVGImageCanvas    canvas = SVGImageCanvas.getBlankCanvas(bounds);
 
-	if (canvas != null) {
-	    Graphics2D g = canvas.createGraphics();
+        if (canvas != null) {
+            Graphics2D g = canvas.createGraphics();
 
-	    for (SVGGenericElement descendants : getDescendants()) {
-		descendantRect = descendants.getBounds();
-		if (descendantRect != null) {
-		    g.drawImage(descendants.draw(), null,
-			    (int) (SVGImageCanvas.getZoomScale() *(descendantRect.x - bounds.x)),
-			    (int) (SVGImageCanvas.getZoomScale() *(descendantRect.y - bounds.y)));
-		}
-	    }
-	}
+            for (SVGGenericElement descendants : getDescendants()) {
+                descendantRect = descendants.getBounds();
 
-	return canvas;
+                if (descendantRect != null) {
+                    g.drawImage(descendants.draw(), null,
+                                (int) (SVGImageCanvas.getZoomScale() * (descendantRect.x - bounds.x)),
+                                (int) (SVGImageCanvas.getZoomScale() * (descendantRect.y - bounds.y)));
+                }
+            }
+        }
+
+        return canvas;
     }
 
     /*
      * METHODS
      */
+
+    /**
+     * Parses the attributes on the  <<code>g</code>> element in the SVG document
+     * @param e lement from the document returned by the XMLParser
+     * @return <code>SVGGElement</code> object
+     */
     public static SVGGElement parseElement(Element e) {
-	SVGGElement g_e = new SVGGElement();
-	g_e.parseAttributes(e);
-	return g_e;
+        SVGGElement g_e = new SVGGElement();
+
+        g_e.parseAttributes(e);
+
+        return g_e;
     }
 }
--- src/rocks6205/svg/elements/SVGSVGElement.java
+++ src/rocks6205/svg/elements/SVGSVGElement.java
@@ -1,132 +1,203 @@
+
 /**
- * 
+ *
  * Class: SVGSVGElement
- * Description: 
- * 
+ * Description:
+ *
  * @author: Cheow Yeong Chi
  * @date: 14/03/2013
- * 
+ *
  */
-
 package rocks6205.svg.elements;
 
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
+//~--- non-JDK imports --------------------------------------------------------
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 import rocks6205.svg.adt.SVGLengthUnit;
+
 import rocks6205.svgFamily.SVGImageCanvas;
 
+//~--- JDK imports ------------------------------------------------------------
 
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * The <code>SVGSVGElement</code> class is a container element
+ * corresponds to the <<code>svg</code>> element in the SVG document.<p>
+ * It can be used to nest a standalone SVG fragment inside the current document
+ * elements.
+ *
+ * @author Cheow Yeong Chi
+ *
+ * @since 1.1
+ *
+ */
 public class SVGSVGElement extends SVGContainerElement {
-    private SVGLengthUnit width = new SVGLengthUnit(1000); ///default to 1000x1000
+
+    /**
+     * Width of SVG fragment
+     */
+    private SVGLengthUnit width = new SVGLengthUnit(1000);    // /default to 1000x1000
+
+    /**
+     * Height of SVG fragment
+     */
     private SVGLengthUnit height = new SVGLengthUnit(1000);
 
+    /**
+     * Construct an instance of <code>SVGSVGElement</code> with <code>width</code> and
+     * <code>height</code>
+     *
+     * @param width
+     * @param height
+     */
     public SVGSVGElement(SVGLengthUnit width, SVGLengthUnit height) {
-	setWidth(width);
-	setHeight(height);
-	setFill(SVG_FILL_DEFAULT);
-	setStroke(SVG_STROKE_DEFAULT);
-	setStrokeWidth(SVG_STROKE_WIDTH_DEFAULT);
-	setStrokeLineCap(SVG_STROKE_LINECAP_DEFAULT);
-	setStrokeLineJoin(SVG_STROKE_LINEJOIN_DEFAULT);
+        setWidth(width);
+        setHeight(height);
+        setFill(SVG_FILL_DEFAULT);
+        setStroke(SVG_STROKE_DEFAULT);
+        setStrokeWidth(SVG_STROKE_WIDTH_DEFAULT);
+        setStrokeLineCap(SVG_STROKE_LINECAP_DEFAULT);
+        setStrokeLineJoin(SVG_STROKE_LINEJOIN_DEFAULT);
     }
 
+    /**
+     * @return Width of SVG fragment
+     */
     public SVGLengthUnit getWidth() {
-	return width;
+        return width;
     }
 
+    /**
+     * @param width Width of SVG fragment
+     */
     public final void setWidth(SVGLengthUnit width) {
-	if (width != null)		this.width = width;
+        if (width != null) {
+            this.width = width;
+        }
     }
 
+    /**
+     * @return Height of SVG fragment
+     */
     public SVGLengthUnit getHeight() {
-	return height;
+        return height;
     }
 
+    /**
+     * @param height Height of SVG fragment
+     */
     public final void setHeight(SVGLengthUnit height) {
-	if (height != null) 	this.height = height;
+        if (height != null) {
+            this.height = height;
+        }
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public Rectangle2D.Float getBounds() {
-	return new Rectangle2D.Float(0, 0, width.getValue(), height.getValue());
+        return new Rectangle2D.Float(0, 0, width.getValue(), height.getValue());
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public SVGImageCanvas draw() {
-	Rectangle2D.Float childRect;
-	SVGImageCanvas svgCanvas = SVGImageCanvas.getBlankCanvas(getBounds());
+        Rectangle2D.Float childRect;
+        SVGImageCanvas    svgCanvas = SVGImageCanvas.getBlankCanvas(getBounds());
 
-	if (svgCanvas != null) {
-	    Graphics2D g = svgCanvas.createGraphics();
+        if (svgCanvas != null) {
+            Graphics2D g = svgCanvas.createGraphics();
 
-	    for (SVGGenericElement child : getDescendants()) {
-		childRect = child.getBounds();
-		if (childRect != null) {
-		    g.drawImage(child.draw(), null, (int) (SVGImageCanvas.getZoomScale() *childRect.x),
-			    (int) (SVGImageCanvas.getZoomScale() *childRect.y));
-		}
-	    }
-	}
+            for (SVGGenericElement child : getDescendants()) {
+                childRect = child.getBounds();
 
-	return svgCanvas;
+                if (childRect != null) {
+                    g.drawImage(child.draw(), null, (int) (SVGImageCanvas.getZoomScale() * childRect.x),
+                                (int) (SVGImageCanvas.getZoomScale() * childRect.y));
+                }
+            }
+        }
+
+        return svgCanvas;
     }
 
+    /**
+     * Parses the attributes and the nested <node>elements</code> of the SVG document,
+     * consequently constructs the vector of elements in a nest.
+     *
+     * @param document <code>Document</code> from XML parsed and returned by the <code>XMLParser</code>
+     * @return <code>SVGSVGElement</code> object
+     */
     public static SVGSVGElement parseDocument(Document document) {
-	SVGSVGElement svg_e = null;
-	SVGContainerElement ancestorElement = null, newAncestorElement;
-	Node node = document.getDocumentElement();
-	Element e;
+        SVGSVGElement       svg_e           = null;
+        SVGContainerElement ancestorElement = null, newAncestorElement;
+        Node                node            = document.getDocumentElement();
+        Element             e;
 
-	while (node != null) {
-	    newAncestorElement = null;
+        while (node != null) {
+            newAncestorElement = null;
 
-	    if (node.getNodeType() == Node.ELEMENT_NODE) {
-		e = (Element) node;
-		switch (e.getTagName()) {
-		case "svg":
-		    SVGLengthUnit width = SVGLengthUnit.parse(e.getAttributeNS(
-			    null, "width"));
-		    SVGLengthUnit height = SVGLengthUnit.parse(e.getAttributeNS(
-			    null, "height"));
-		    svg_e = new SVGSVGElement(width, height);
-		    ancestorElement = svg_e;
-		    break;
-		case "g":
-		    SVGGElement g_e = SVGGElement.parseElement(e);
-		    ancestorElement.addDescendant(g_e);
-		    newAncestorElement = g_e;
-		    break;
-		case "rect":
-		    ancestorElement.addDescendant(SVGRectElement.parseElement(e));
-		    break;
-		case "circle":
-		    ancestorElement.addDescendant(SVGCircleElement.parseElement(e));
-		    break;
-		case "line":
-		    ancestorElement.addDescendant(SVGLineElement.parseElement(e));
-		    break;
-		}
-	    }
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                e = (Element) node;
 
-	    if (node.hasChildNodes()) {
-		node = node.getFirstChild();
-		if (newAncestorElement != null) {
-		    ancestorElement = newAncestorElement;
-		}
-	    } else if (node.getNextSibling() != null) {
-		node = node.getNextSibling();
-	    } else {
-		node = node.getParentNode().getNextSibling();
-		if (ancestorElement.getAncestorElement() != null) {
-		    ancestorElement = ancestorElement.getAncestorElement();
-		}
-	    }
-	}
-	return svg_e;
+                switch (e.getTagName()) {
+                case "svg" :
+                    SVGLengthUnit width  = SVGLengthUnit.parse(e.getAttributeNS(null, "width"));
+                    SVGLengthUnit height = SVGLengthUnit.parse(e.getAttributeNS(null, "height"));
+
+                    svg_e           = new SVGSVGElement(width, height);
+                    ancestorElement = svg_e;
+
+                    break;
+
+                case "g" :
+                    SVGGElement g_e = SVGGElement.parseElement(e);
+
+                    ancestorElement.addDescendant(g_e);
+                    newAncestorElement = g_e;
+
+                    break;
+
+                case "rect" :
+                    ancestorElement.addDescendant(SVGRectElement.parseElement(e));
+
+                    break;
+
+                case "circle" :
+                    ancestorElement.addDescendant(SVGCircleElement.parseElement(e));
+
+                    break;
+
+                case "line" :
+                    ancestorElement.addDescendant(SVGLineElement.parseElement(e));
+
+                    break;
+                }
+            }
+
+            if (node.hasChildNodes()) {
+                node = node.getFirstChild();
+
+                if (newAncestorElement != null) {
+                    ancestorElement = newAncestorElement;
+                }
+            } else if (node.getNextSibling() != null) {
+                node = node.getNextSibling();
+            } else {
+                node = node.getParentNode().getNextSibling();
+
+                if (ancestorElement.getAncestorElement() != null) {
+                    ancestorElement = ancestorElement.getAncestorElement();
+                }
+            }
+        }
+
+        return svg_e;
     }
 }
