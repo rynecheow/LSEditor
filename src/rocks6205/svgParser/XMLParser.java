1057fe8775a781430a08821327585d473e1ccc2c
Ryne Cheow <rynec92@hotmail.com>
2013-03-28 10:48:17 +0800

    refactor SVGPARSER package completed

--- src/rocks6205/svgParser/XMLParser.java
+++ src/rocks6205/svgParser/XMLParser.java
@@ -1,14 +1,14 @@
-/**
- * 
- * Class: XMLParser
- * Description: Parsing a single XML source into a Document ready to be read by other classes.
- * 
- * @author: Cheow Yeong Chi
- * @date: 11/03/2013
- * 
- */
-
 package rocks6205.svgParser;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.w3c.dom.Document;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+//~--- JDK imports ------------------------------------------------------------
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -17,18 +17,24 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.Document;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+/**
+ *
+ * Parsing a single XML source into a Document ready to be read by other classes.
+ *
+ * @author Cheow Yeong Chi
+ * @since 1.0
+ *
+ */
 public class XMLParser {
 
     /*
      * CONSTRUCTOR
      */
-    private XMLParser() {
-	// TODO Auto-generated constructor stub
-    }
+
+    /**
+     * Default constructor
+     */
+    private XMLParser() {}
 
     /*
      * METHODS
@@ -36,30 +42,31 @@ public class XMLParser {
 
     /**
      * Checks and build a document from input source and aware of namespaces.
-     * 
+     *
      * @param XMLInputSource
      * @return XML Document to be further processed.
      */
-    public static Document parseXml(InputSource XMLInputSource){
-	Document doc = null;		
-	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	factory.setNamespaceAware(true);
-	try {
-	    DocumentBuilder builder = factory.newDocumentBuilder();
-	    // Prevent loading external DTD
-	    builder.setEntityResolver(new EntityResolver() {
-		public InputSource resolveEntity(String publicId,
-			String systemId) throws SAXException, IOException {
-		    return new InputSource(new StringReader(""));
-		}
-	    });
+    public static Document parseXml(InputSource XMLInputSource) {
+        Document               doc     = null;
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
-	    doc = builder.parse(XMLInputSource);
-	} catch (IOException ioe) {
-	} catch (SAXException sae) {
-	} catch (ParserConfigurationException pce) {
-	} catch (NullPointerException npe) {}
+        factory.setNamespaceAware(true);
 
-	return doc;
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            // Prevent loading external DTD
+            builder.setEntityResolver(new EntityResolver() {
+                public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+                    return new InputSource(new StringReader(""));
+                }
+            });
+            doc = builder.parse(XMLInputSource);
+        } catch (IOException ioe) {}
+        catch (SAXException sae) {}
+        catch (ParserConfigurationException pce) {}
+        catch (NullPointerException npe) {}
+
+        return doc;
     }
 }
