aa91ea4c6b9e58046a887a10c3294a438d2ed9af
Ryne Cheow <rynec92@hotmail.com>
2013-03-28 10:46:45 +0800

    refactor SVGFAMILY class completed

--- src/rocks6205/svgFamily/SVGCanvasProperties.java
+++ src/rocks6205/svgFamily/SVGCanvasProperties.java
@@ -1,44 +1,71 @@
-/**
- * 
- * Class: SVGCanvasProperties
- * Description: Describes the environment where the canvas will run on, focusing on output resolution and text size.
- * 
- * @author: Cheow Yeong Chi
- * @date: 11/03/2013
- * 
- */
 package rocks6205.svgFamily;
 
+/**
+ *
+ * Describes the environment where the canvas will run on, focusing on output resolution and text size.
+ *
+ * @author Cheow Yeong Chi
+ * @since 1.3
+ *
+ */
 public class SVGCanvasProperties {
+
     /*
      * PROPERTIES
      */
+
+    /**
+     * Text size
+     */
     private static float TEXT_SIZE = 10;
+
+    /**
+     * Resolution in DPI
+     */
     private static int DOTS_PER_INCH = 72;
 
     /*
      * CONSTRUCTOR
      */
-    private SVGCanvasProperties() {
-    }
+
+    /**
+     * Default constructor
+     */
+    private SVGCanvasProperties() {}
+
     /*
      * ACCESSORS
      */
+
+    /**
+     * @return Resolution in DPI
+     */
     public static int getOutputResolution() {
-	return DOTS_PER_INCH;
+        return DOTS_PER_INCH;
     }
 
+    /**
+     * @return Text size
+     */
     public static float getFontSize() {
-	return TEXT_SIZE;
+        return TEXT_SIZE;
     }
+
     /*
      * MUTATORS
      */
+
+    /**
+     * @param dpi Resolution in DPI
+     */
     public static void setOutputResolution(int dpi) {
-	DOTS_PER_INCH = dpi;
+        DOTS_PER_INCH = dpi;
     }
 
-    public static void setFontSize(float fontSize) {
-	TEXT_SIZE = fontSize;
+    /**
+     * @param fontSize Text size
+     */
+    public static void setFontSize(float textSize) {
+        TEXT_SIZE = textSize;
     }
 }
--- src/rocks6205/svgFamily/SVGImageCanvas.java
+++ src/rocks6205/svgFamily/SVGImageCanvas.java
@@ -1,47 +1,85 @@
-/**
- * 
- * Class: SVGImageCanvas
- * Description: 
- * 
- * @author: Cheow Yeong Chi
- * @date: 11/03/2013
- * 
- */
-
 package rocks6205.svgFamily;
+
+//~--- JDK imports ------------------------------------------------------------
 
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
+/**
+ *
+ * The <code>SVGImageCanvas</code> is a subtype of <code>BufferedImage</code>
+ * where zooming is allowed and preset with <code>width</code>, <code>height</code>
+ * and color profile.
+ *
+ * @author Cheow Yeong Chi
+ * @since 1.3
+ *
+ */
 public class SVGImageCanvas extends BufferedImage {
 
+    /**
+     * Default minimum zoom scale
+     */
     public static final int DEFAULT_MIN_ZOOM_LEVEL = -20;
+
+    /**
+     * Default maximum zoom scale
+     */
     public static final int DEFAULT_MAX_ZOOM_LEVEL = 10;
+
+    /**
+     * Default zoom multiplication factor
+     */
     public static final double DEFAULT_ZOOM_MULTIPLICATION_FACTOR = 1.0f;
 
+    /**
+     * Zoom scale of current instance
+     */
     private static double zoomScale = DEFAULT_ZOOM_MULTIPLICATION_FACTOR;
 
+    /*
+     * CONSTRUCTOR
+     */
+
+    /**
+     * Constructs an <code>SVGImageCanvas</code> object with defined
+     * <code>height</code> and <code>width</code>.
+     * @param width
+     * @param height
+     */
     public SVGImageCanvas(int width, int height) {
-	super(width, height, BufferedImage.TYPE_4BYTE_ABGR_PRE);
+        super(width, height, BufferedImage.TYPE_4BYTE_ABGR_PRE);
     }
 
+    /**
+     * Get a blank canvas with predefined <code>height</code> and <code>width</code>, and
+     * defined rectangular <code>bounds</code>.
+     *
+     * @param bounds Bounds of current component.
+     * @return Blank canvas
+     */
     public static SVGImageCanvas getBlankCanvas(Rectangle2D.Float bounds) {
-	boolean shouldGetCanvas = (bounds != null)  && (bounds.width > 0 && bounds.height > 0);
+        boolean shouldGetCanvas = (bounds != null) && ((bounds.width > 0) && (bounds.height > 0));
 
-	if (shouldGetCanvas) {
-	    return new SVGImageCanvas(
-		    (int) (SVGImageCanvas.getZoomScale() * Math.ceil(bounds.width)),
-		    (int) (SVGImageCanvas.getZoomScale() * Math.ceil(bounds.height)));
-	}
-	return null;
+        if (shouldGetCanvas) {
+            return new SVGImageCanvas((int) (SVGImageCanvas.getZoomScale() * Math.ceil(bounds.width)),
+                                      (int) (SVGImageCanvas.getZoomScale() * Math.ceil(bounds.height)));
+        }
 
+        return null;
     }
 
+    /**
+     * @return Zoom scale of current instance
+     */
     public static double getZoomScale() {
-	return zoomScale;
+        return zoomScale;
     }
 
+    /**
+     * @param zoomScale Zoom scale of current instance
+     */
     public static void setZoomScale(double zoomScale) {
-	SVGImageCanvas.zoomScale = zoomScale;
+        SVGImageCanvas.zoomScale = zoomScale;
     }
 }
