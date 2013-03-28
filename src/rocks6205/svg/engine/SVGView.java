374fd05636d0d8382a1bbb02e9191880d0c68f30
Ryne Cheow <rynec92@hotmail.com>
2013-03-28 09:22:45 +0800

    refactor ENGINE package completed

--- src/rocks6205/svg/engine/SVGView.java
+++ src/rocks6205/svg/engine/SVGView.java
@@ -1,13 +1,26 @@
 package rocks6205.svg.engine;
 
+//~--- non-JDK imports --------------------------------------------------------
+
+import rocks6205.svg.engine.events.SVGViewPanMouseAdaptor;
+import rocks6205.svg.engine.viewcomponents.SVGViewBottomToolbar;
+import rocks6205.svg.engine.viewcomponents.SVGViewDeleteAccessoryPanel;
+import rocks6205.svg.engine.viewcomponents.SVGViewMenubar;
+import rocks6205.svg.engine.viewcomponents.SVGViewTopToolbar;
+import rocks6205.svg.engine.viewcomponents.SVGViewport;
+
+import rocks6205.svgFamily.SVGImageCanvas;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Rectangle;
-import java.awt.event.MouseAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import java.util.Observable;
 import java.util.Observer;
 
@@ -17,166 +30,181 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
 
-import rocks6205.svg.engine.events.SVGViewPanMouseAdaptor;
-import rocks6205.svg.engine.viewcomponents.SVGViewBottomToolbar;
-import rocks6205.svg.engine.viewcomponents.SVGViewDeleteAccessoryPanel;
-import rocks6205.svg.engine.viewcomponents.SVGViewMenubar;
-import rocks6205.svg.engine.viewcomponents.SVGViewTopToolbar;
-import rocks6205.svg.engine.viewcomponents.SVGViewport;
-import rocks6205.svgFamily.SVGImageCanvas;
-
 /**
  * A class defining how the main user interface should look like.
  * <p>
  *
  * @author Toh Huey Jing
  * @author Cheow Yeong Chi
- * 
- * @date 17/03/2013
- *
  *
  * @since 1.2
  */
 public class SVGView extends JFrame implements Observer {
-
     private static final long serialVersionUID = 6764861773639452353L;
 
     /*
      * PROPERTIES
      */
+
+    /**
+     * Model object
+     */
     private SVGModel model;
+
+    /**
+     * Controller object
+     */
     private SVGViewController controller;
 
     /*
      * GUI COMPONENTS
      */
-    SVGViewMenubar menuBar;
-    SVGViewTopToolbar topTool;
-    SVGViewBottomToolbar bottomTool;
+    SVGViewMenubar              menuBar;
+    SVGViewTopToolbar           topTool;
+    SVGViewBottomToolbar        bottomTool;
     SVGViewDeleteAccessoryPanel delete;
+    JPanel                      panelTop, panelBottom;
+    SVGViewport                 renderPanel;
+    JViewport                   viewport;
+    Container                   container;
+    JScrollPane                 scrollPane;
+    Dimension                   renderAreaSize;
 
-    JPanel panelTop, panelBottom;
-    SVGViewport renderPanel;
-    JViewport viewport;
-    Container container = getContentPane();
-    JScrollPane scrollPane;
-    Dimension renderAreaSize;
     /*
      * ACTION COMPONENTS
      */
-    private MouseAdapter panListener;
+
+    /**
+     * Handle pan events.
+     */
+    private SVGViewPanMouseAdaptor panListener;
+
     /*
      * CONSTRUCTOR
      */
     public SVGView() {
-	super();
-	initialise();
-	customise();
+        super();
+        initialise();
+        customise();
     }
 
     /**
-     * Initialisation of GUI components
+     * Initialisation of GUI components.
      */
     private void initialise() {
-	renderAreaSize = new Dimension(0,0);
-	menuBar = new SVGViewMenubar(this);
-	topTool = new SVGViewTopToolbar(this);
-	bottomTool = new SVGViewBottomToolbar(this);
-	delete = new SVGViewDeleteAccessoryPanel(this);
-
-	panListener = new SVGViewPanMouseAdaptor();
-
-	renderPanel = new SVGViewport(this);
-	panelTop = new JPanel();
-	panelBottom = new JPanel();
-	scrollPane = new JScrollPane(renderPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-	viewport = new JViewport();
-	viewport = scrollPane.getViewport();
-	viewport.addMouseListener(panListener);
-	viewport.addMouseMotionListener(panListener);
+        menuBar        = new SVGViewMenubar(this);
+        topTool        = new SVGViewTopToolbar(this);
+        bottomTool     = new SVGViewBottomToolbar(this);
+        delete         = new SVGViewDeleteAccessoryPanel(this);
+        panelTop       = new JPanel();
+        panelBottom    = new JPanel();
+        renderPanel    = new SVGViewport(this);
+        viewport       = new JViewport();
+        container      = getContentPane();
+        renderAreaSize = new Dimension(0, 0);
+        scrollPane     = new JScrollPane(renderPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                                         JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        panListener = new SVGViewPanMouseAdaptor();
     }
 
     /**
-     * Customisation of GUI components
-     * 
+     * Customisation of GUI components.
+     *
      */
     private void customise() {
-	setSize(1000,1000);
-	setTitle("SVG Editor");
-	scrollPane.setBounds(new Rectangle(renderAreaSize));
-	renderPanel.setBackground(Color.WHITE);
-
-	container.setLayout(new BorderLayout());
-	panelTop.setLayout(new BorderLayout());
-	panelBottom.setLayout(new BorderLayout());
-
-	panelTop.add(topTool, BorderLayout.WEST);
-	panelBottom.add(bottomTool, BorderLayout.WEST);
-	panelBottom.add(delete, BorderLayout.EAST);	
-
-	setVisible(true);
-	setMinimumSize(new Dimension(700,700));
-	setClosingEvent();
-	setJMenuBar(menuBar);
-
-	container.add(scrollPane, BorderLayout.CENTER);
-	container.add(panelTop, BorderLayout.NORTH);
-	container.add(panelBottom, BorderLayout.SOUTH);
+        setSize(1000, 1000);
+        setTitle("SVG Editor");
+        scrollPane.setBounds(new Rectangle(renderAreaSize));
+        renderPanel.setBackground(Color.WHITE);
+        container.setLayout(new BorderLayout());
+        panelTop.setLayout(new BorderLayout());
+        panelBottom.setLayout(new BorderLayout());
+        panelTop.add(topTool, BorderLayout.WEST);
+        panelBottom.add(bottomTool, BorderLayout.WEST);
+        panelBottom.add(delete, BorderLayout.EAST);
+        setVisible(true);
+        setMinimumSize(new Dimension(700, 700));
+        setClosingEvent();
+        setJMenuBar(menuBar);
+        container.add(scrollPane, BorderLayout.CENTER);
+        container.add(panelTop, BorderLayout.NORTH);
+        container.add(panelBottom, BorderLayout.SOUTH);
+        viewport = scrollPane.getViewport();
+        viewport.addMouseListener(panListener);
+        viewport.addMouseMotionListener(panListener);
     }
 
     /**
      * Prompt exit confirmation while user clicks on 'x' button on the window
-     * 
      */
     private void setClosingEvent() {
-	addWindowListener(new WindowAdapter() {
-	    @Override
-	    public void windowClosing(WindowEvent winEvt) {
-		int closeCf = JOptionPane.showConfirmDialog(null, "Exit SVG Editor?", "Confirm exit", JOptionPane.WARNING_MESSAGE);
-		if (closeCf == JOptionPane.YES_OPTION) {
-		    System.exit(0);
-		}
-	    }
-	});
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent winEvt) {
+                int closeCf = JOptionPane.showConfirmDialog(null, "Exit SVG Editor?", "Confirm exit",
+                                  JOptionPane.WARNING_MESSAGE);
+
+                if (closeCf == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
     }
+
     /*
      * ACCESSORS
      */
+
+    /**
+     * @return Model object
+     */
     public SVGModel getModel() {
-	return model;
+        return model;
     }
 
+    /**
+     * @return Controller object
+     */
     public SVGViewController getController() {
-	return controller;
+        return controller;
     }
 
     /*
      * MUTATORS
      */
+
+    /**
+     * @param controller Controller object
+     */
     public void setController(SVGViewController controller) {
-	this.controller = controller;
+        this.controller = controller;
     }
 
+    /**
+     * @param model Model object
+     */
     public void setModel(SVGModel model) {
-	this.model = model;
+        this.model = model;
     }
 
-    @Override
+    /**
+     * <p>Updates the image canvas whenever a change is made to the observed object
+     * which is the <code>SVGModel.</code></p>
+     * {@inheritDoc}
+     */
     public void update(Observable o, Object arg) {
-	if (o instanceof SVGModel) {
-	    float w = ((SVGModel) o).getSVGElement().getWidth().getValue();
-	    float h = ((SVGModel) o).getSVGElement().getHeight().getValue();
-	    renderAreaSize.height = (int) h;
-	    renderAreaSize.width = (int) w;
-	    scrollPane.setPreferredSize(renderAreaSize);
-	    renderPanel.setPreferredSize(renderAreaSize);
+        if (o instanceof SVGModel) {
+            float w = ((SVGModel) o).getSVGElement().getWidth().getValue();
+            float h = ((SVGModel) o).getSVGElement().getHeight().getValue();
 
-	    renderPanel.setCanvas((SVGImageCanvas) arg);
-	    renderPanel.repaint();
-	    scrollPane.setViewportView(renderPanel);
-	    scrollPane.repaint();
-	}
-
-
+            renderAreaSize.height = (int) h;
+            renderAreaSize.width  = (int) w;
+            scrollPane.setPreferredSize(renderAreaSize);
+            renderPanel.setPreferredSize(renderAreaSize);
+            renderPanel.setCanvas((SVGImageCanvas) arg);
+            renderPanel.repaint();
+            scrollPane.setViewportView(renderPanel);
+            scrollPane.repaint();
+        }
     }
 }
--- src/rocks6205/svg/engine/SVGViewController.java
+++ src/rocks6205/svg/engine/SVGViewController.java
@@ -1,81 +1,125 @@
-/**
- * 
- * Class: SVGViewController
- * Description: 
- * 
- * @author: Cheow Yeong Chi
- * @date: 14/03/2013
- * 
- */
-
 package rocks6205.svg.engine;
-import java.io.File;
+
+//~--- non-JDK imports --------------------------------------------------------
 
 import org.w3c.dom.Document;
+
 import org.xml.sax.InputSource;
 
 import rocks6205.svg.elements.SVGSVGElement;
+
 import rocks6205.svgParser.XMLParser;
 
-public class SVGViewController{
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.File;
+
+/**
+ * The <code>SVGViewController</code> class defines a MVC module and plays as
+ * the central control of the whole engine which lets the model and view
+ * communicate effectively.
+ *
+ * @author: Cheow Yeong Chi
+ * @since 1.2
+ *
+ */
+public class SVGViewController {
+
     /*
      * PROPERTIES
      */
+
+    /**
+     * Model object
+     */
     private SVGModel model;
+
+    /**
+     * View object
+     */
     private SVGView view;
 
     /*
      * CONSTRUCTOR
      */
+
+    /**
+     * Constructs an <code>SVGViewController</code> instances with
+     * <code>model</code> and <code>view</code>.
+     * @param model  Model object
+     * @param view  View object
+     */
     public SVGViewController(SVGModel model, SVGView view) {
-	setModel(model);
-	setView(view);
+        setModel(model);
+        setView(view);
     }
 
     /*
      * ACCESSORS
      */
+
+    /**
+     * @return View object
+     */
     public SVGView getView() {
-	return view;
+        return view;
     }
 
+    /**
+     * @return Model object
+     */
     public SVGModel getModel() {
-	return model;
+        return model;
     }
 
     /*
      * MUTATORS
      */
+
+    /**
+     * @param model Model object
+     */
     public void setModel(SVGModel model) {
-	this.model = model;
+        this.model = model;
     }
 
-
+    /**
+     * @param view View object
+     */
     public void setView(SVGView view) {
-	this.view = view;
+        this.view = view;
     }
-
 
     /*
      * METHODS
      */
 
+    /**
+     * Loads file from two ways:<p>
+     * 1) Read directly from <code>JFileChooser</code>.<br>
+     * 2) File from path string passed by command line argument.<p>
+     *
+     * File is then parsed into a <code>Document</code> object and rendered.
+     *
+     * @param file
+     */
     public void fileLoad(File file) {
-	if (file != null) {
-	    String path = file.toURI().toString();
-	    Document doc = null;
+        if (file != null) {
+            String   path = file.toURI().toString();
+            Document doc  = null;
 
-	    try{
-		doc = XMLParser.parseXml(new InputSource(path));
-	    }catch(NullPointerException npe){
-		npe.printStackTrace();
-	    }
+            try {
+                doc = XMLParser.parseXml(new InputSource(path));
+            } catch (NullPointerException npe) {
+                npe.printStackTrace();
+            }
 
-	    if (doc != null) {
-		SVGSVGElement svg_e = SVGSVGElement.parseDocument(doc);
-		model.setSVGElement(svg_e);
-		model.render();
-	    }
-	}
+            if (doc != null) {
+                SVGSVGElement svg_e = SVGSVGElement.parseDocument(doc);
+
+                model.setSVGElement(svg_e);
+                model.render();
+            }
+        }
     }
 }
