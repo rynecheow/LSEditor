53804fb595e6d58dc62aa5f14858ca89be2e6d5e
Ryne Cheow <rynec92@hotmail.com>
2013-04-07 11:45:57 +0800

    changed @since

--- src/rocks6205/svg/editor/events/SVGEditorDrawMouseAdaptor.java
+++ src/rocks6205/svg/editor/events/SVGEditorDrawMouseAdaptor.java
@@ -1,50 +1,52 @@
 package rocks6205.svg.editor.events;
 
-import java.awt.Cursor;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
+//~--- non-JDK imports --------------------------------------------------------
 
-import javax.swing.JComponent;
-import javax.swing.JViewport;
+import rocks6205.svg.editor.viewcomponents.SVGEditorEditingPanel;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.awt.event.MouseEvent;
 
 /**
  *
  * Handles event for drawing 3 different shapes on canvas
  * <p>
  *
- * @author: Suagr CheeSheen Chan
+ * @author: Sugar CheeSheen Chan
  *
- * @since 1.0
+ * @since 2.1
  *
  */
 public class SVGEditorDrawMouseAdaptor {
-    
+
     /**
      * {@inheritDoc}<p>
-     * 
+     *
      */
     public void mousePressed(MouseEvent event) {
-	System.out.printf("Pressed at x:%d y:%d" , 
-		event.getX() , event.getY() );
+        SVGEditorEditingPanel editor = (SVGEditorEditingPanel) event.getSource();
+
+        System.out.printf("Pressed at x:%d y:%d", event.getX(), event.getY());
     }
 
     /**
      * {@inheritDoc}<p>
-     * 
+     *
      */
     public void mouseReleased(MouseEvent event) {
-	System.out.printf("Released at x:%d y:%d" , 
-		event.getX() , event.getY() );
+        SVGEditorEditingPanel editor = (SVGEditorEditingPanel) event.getSource();
+
+        System.out.printf("Released at x:%d y:%d", event.getX(), event.getY());
     }
 
     /**
      * {@inheritDoc}<p>
-     * 
+     *
      */
     public void mouseDragged(MouseEvent event) {
-	System.out.printf("Dragged at x:%d y:%d" , 
-		event.getX() , event.getY() );
-    }
+        SVGEditorEditingPanel editor = (SVGEditorEditingPanel) event.getSource();
 
+        System.out.printf("Dragged at x:%d y:%d", event.getX(), event.getY());
+    }
 }
