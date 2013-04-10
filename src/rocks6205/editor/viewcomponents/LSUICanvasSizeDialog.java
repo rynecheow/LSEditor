aeac8226ed23a6cfd2610d3fe578f9b64a22195c
Ryne Cheow <rynec92@hotmail.com>
2013-04-10 13:07:51 +0800

    new file dialog

---
+++ src/rocks6205/editor/viewcomponents/LSUICanvasSizeDialog.java
@@ -0,0 +1,37 @@
+package rocks6205.editor.viewcomponents;
+
+
+import java.awt.Dimension;
+import javax.swing.JDialog;
+
+public final class LSUICanvasSizeDialog extends JDialog implements LSUIProtocol {
+
+    /*
+     * SIZE PROPERTIES
+     */
+    int       width  = 600;
+    int       height = 450;
+    Dimension screen;
+    int       x;
+    int       y;
+    
+    private static final long serialVersionUID = 4595939286877280108L;
+
+    public LSUICanvasSizeDialog() {
+	// TODO Auto-generated constructor stub
+    }
+
+
+    @Override
+    public void initialise() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void customise() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
