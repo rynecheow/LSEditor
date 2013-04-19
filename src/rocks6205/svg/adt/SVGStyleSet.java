2e091ad631e6212cdfc068af933ca302ef6b51bc
Ryne Cheow <rynec92@hotmail.com>
2013-04-19 21:16:25 +0800

    Revert "revert new classes"
    This reverts commit 24fb413d6036c7856da35749c1c62954884cb23f.

---
+++ src/rocks6205/svg/adt/SVGStyleSet.java
@@ -0,0 +1,35 @@
+
+/**
+ *
+ */
+package rocks6205.svg.adt;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.HashMap;
+
+/**
+ * @author Komalah
+ *
+ */
+public class SVGStyleSet {
+
+    private final HashMap<String, String> mStyleMap = new HashMap<String, String>();
+
+    
+    public SVGStyleSet(final String pString) {
+            final String[] styles = pString.split(";");
+            for (final String s : styles) {
+                    final String[] style = s.split(":");
+                    if (style.length == 2) {
+                            this.mStyleMap.put(style[0], style[1]);
+                    }
+            }
+    }
+
+    //Getter and setter
+
+    public String getStyle(final String pStyleName) {
+            return this.mStyleMap.get(pStyleName);
+    }
+}
